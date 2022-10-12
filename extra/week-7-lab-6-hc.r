3dc621e1fefc9b30099ef514f1b94dc21aac7bcc
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-10-12 09:42:14 +0200

    content: format hc better

--- extra/week-7-lab-6-hc.r
+++ extra/week-7-lab-6-hc.r
@@ -5,6 +5,8 @@
 # Modified:  2022-10-11
 # Notes:     refer to macKinnonWhite:1985 Some heteroskedasticity-consistent covariance matrix estimators
 
+# Preliminaries ---------------------------------------------------------------
+
 # Fit some model
 lmfit <- lm(mpg ~ ., mtcars)
 
@@ -25,25 +27,38 @@ Hmat <- X %*% solve(t(X) %*% X) %*% t(X)
 
 # Compute residuals (aka u_hat)
 cbind(
-    lm = resid(lmfit),
-    Hmat = drop((diag(1, n) - Hmat) %*% y)
+    R = resid(lmfit),
+    manual = drop((diag(1, n) - Hmat) %*% y)
 )
 u_hat <- resid(lmfit)
 
 # Compute error variance estiamte (aka Sigma^2)
 c(
-    lm = sigma(lmfit)^2,
-    resid = (sum(resid(lmfit)^2)) / (n - p - 1),
-    Hmat = t(u_hat) %*% u_hat / (n - p - 1)
+    R = sigma(lmfit)^2,
+    manual = (sum(resid(lmfit)^2)) / (n - p - 1),
+    manual2 = t(u_hat) %*% u_hat / (n - p - 1)
 )
 sigma2 <- sigma(lmfit)^2
 
-# Compute standard variance-covariance matrix
-vcov_mat <- sigma2 * solve(t(X) %*% X)
-round(vcov_mat - vcov(lmfit), 5)
+# Compute standard variance-covariance matrix ---------------------------------
 
-# Compute hetheroskedastic variance-covariance matrix
-# Hetheroskedastic version
+# R result
+vcov_mat_r <- vcov(lmfit)
+
+# Standard manual way
+vcov_mat_man <- sigma2 * solve(t(X) %*% X)
+
+# General manual way
+Omega <- diag(sigma2, n)
+vcov_mat_HC_st <- solve(t(X) %*% X) %*% t(X) %*% Omega %*% X %*% solve(t(X) %*% X)
+
+# Compare
+round(vcov_mat_man - vcov_mat_r, 5)
+round(vcov_mat_HC_st - vcov_mat_r, 5)
+
+# Compute hetheroskedastic variance-covariance matrix -------------------------
+
+# Define omega based on the residuals
 Omega <- diag(u_hat^2)
 
 # HC
@@ -52,19 +67,10 @@ vcov_mat_HC <-  solve(t(X) %*% X) %*% t(X) %*% Omega %*% X %*% solve(t(X) %*% X)
 # HC1
 vcov_mat_HC_df <- n / (n - p - 1) * solve(t(X) %*% X) %*% t(X) %*% Omega %*% X %*% solve(t(X) %*% X)
 
-# Compare sntadard errors obtained with each
+# Compare standard errors obtained with each
 cbind(
     HC1 = sqrt(diag(vcovHC(lmfit, type = "HC"))),
     HC1 = sqrt(diag(vcov_mat_HC)),
     HC2 = sqrt(diag(vcovHC(lmfit, type = "HC1"))),
     HC2 = sqrt(diag(vcov_mat_HC_df))
-)
-
-# HC becomes simple covariance matrix when homoschedasticity is there
-Omega_standard <- diag(sigma2, n)
-vcov_mat_HC_st <- solve(t(X) %*% X) %*% t(X) %*% Omega_standard %*% X %*% solve(t(X) %*% X)
-
-cbind(
-    st = sqrt(diag(vcov_mat)),
-    st = sqrt(diag(vcov_mat_HC_st))
 )

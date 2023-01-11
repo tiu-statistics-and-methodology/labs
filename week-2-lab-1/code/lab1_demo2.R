8482cffc7fd00aac1c54c6678b0714958e1fd0ab
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-01-11 10:14:05 +0100

    hk: improve formatting of week 2 lab 1

--- week-2-lab-1/code/lab1_demo2.R
+++ week-2-lab-1/code/lab1_demo2.R
@@ -1,31 +1,32 @@
-### Title:    Stats & Methods Lab 1 Demonstration Script 2
-### Author:   Kyle M. Lang
-### Created:  2018-09-10
-### Modified by L.V.D.E. Vogelsmeier: 2022-01-25
+# Title:    Stats & Methods Lab 1 Demonstration Script 2
+# Author:   Kyle M. Lang, L.V.D.E. Vogelsmeier, Edo
+# Created:  2018-09-10
+# Modified: 2023-01-10
 
-## Clear the workspace:
+# Set up -----------------------------------------------------------------------
+
+# Clear the workspace:
 rm(list = ls(all = TRUE))
 
-## Set your working directory to the "code" directory:
+# Set your working directory to the "code" directory:
 setwd("")
 
-## Install the 'mice' function:
+# Install the 'mice' function:
 install.packages("mice", repos = "http://cloud.r-project.org")
 
-## Load packages:
+# Load packages
 library(mice) # For missing data descriptives
 library(MASS) # For robust stats
 
-## Define the data directory:
+# Define the data directory:
 dataDir <- "../data/"
 
-###--------------------------------------------------------------------------###
+# 1. Missing data descriptives -------------------------------------------------
 
-### Missing Data Descriptives ###
-
+# Read Big Five Invetory (BFI) data from folder
 bfi <- readRDS(paste0(dataDir, "bfiANC.rds"))
 
-## Compute variable-wise counts/percents missing/observed:
+# Compute variable-wise counts/percents missing/observed:
 cm <- colSums(is.na(bfi))
 cm
 pm <- colMeans(is.na(bfi))
@@ -42,140 +43,146 @@ nrow(bfi) - cm
 nrow(bfi) - co
 1 - po
 
-## Summarize PM:
+# Summarize PM:
 range(pm)
 mean(pm)
 median(pm)
 
-## Find variables with PM greater than 10%:
+# Find variables with PM greater than 10%:
 pm[pm > 0.1]
 
-## Find missing data patterns:
+# Find missing data patterns:
 missPat <- md.pattern(bfi)
 missPat
 
-## Extract the variablewise missing counts:
+# Extract the variablewise missing counts:
 missPat[nrow(missPat), ]
 missPat[nrow(missPat), -ncol(missPat)]
 
-## Extract the patternwise missing counts:
+# Extract the patternwise missing counts:
 missPat[ , ncol(missPat)]
 missPat[-nrow(missPat), ncol(missPat)]
 
-## Extract the counts of pattern membership:
+# Extract the counts of pattern membership:
 as.numeric(rownames(missPat))
 as.numeric(rownames(missPat))[-nrow(missPat)]
 
-## Close the graphics device:
+# Close the graphics device:
 dev.off()
 
-## Compute covariance coverage:
+# Compute covariance coverage:
 cc <- md.pairs(bfi)$rr / nrow(bfi)
 cc
 
-## Extract unique coverage values from the covariance coverage matrix:
+# Extract unique coverage values from the covariance coverage matrix:
 cc2 <- cc[lower.tri(cc)]
 
-## Note that we generally don't include the diagonal elements. The diagonal of 
-## the covariance coverage matrix contains the proportion of observed cases for 
-## each variable. You could, however, obtain the unique coverage value including 
-## the diagonal with:
+# Note that we generally don't include the diagonal elements. The diagonal of 
+# the covariance coverage matrix contains the proportion of observed cases for 
+# each variable. You could, however, obtain the unique coverage value including 
+# the diagonal with:
 cc[lower.tri(cc, diag = TRUE)]
 
-## Obtain the range of the coverage values (use the unique values, excluding 
-## the diagonal):
+# Obtain the range of the coverage values (use the unique values, excluding 
+# the diagonal):
 range(cc2)
 
-## Check that coverage exceeds some threshold:
+# Check that coverage exceeds some threshold:
 eps <- 0.8
 all(cc2 > eps)
 
-## Find problematic pairs (use entire covariance coverage matrix):
+# Find problematic pairs (use entire covariance coverage matrix):
 pat <- cc <= eps
 apply(pat, 1, function(x) names(x)[x])
-###--------------------------------------------------------------------------###
 
-### Outlier Analysis ###
+# 2. Outlier analysis (univariate) ---------------------------------------------
 
+# Load SAT-test data from folder
 tests <- readRDS(paste0(dataDir, "tests.rds")) 
 
-## Create a boxplot of SATV:
+# > 2.1 Boxplot method ---------------------------------------------------------
+
+# Create a boxplot of SATV:
 boxplot(tests$SATV)
 
-## Compute the statistics underlying a boxplot:
+# Compute the statistics underlying a boxplot:
 boxplot.stats(tests$SATV)
 
-## Assign function argument to demonstrate its internals:
+# Assign function argument to demonstrate its internals:
 x <- tests$SATV
 
-## Define a function to implement the boxplot method:
+# Define a function to implement the boxplot method:
 bpOutliers <- function(x) {
-    ## Compute inner and outer fences:
+    # Compute inner and outer fences:
     iFen <- boxplot.stats(x, coef = 1.5)$stats[c(1, 5)]
     oFen <- boxplot.stats(x, coef = 3.0)$stats[c(1, 5)]
     
-    ## Return the row indices of flagged 'possible' and 'probable' outliers:
+    # Return the row indices of flagged 'possible' and 'probable' outliers:
     list(possible = which(x < iFen[1] | x > iFen[2]),
          probable = which(x < oFen[1] | x > oFen[2])
          )
 }
 
+# Remove objects used to demonstrate what happens inside the function
 rm(list = c("x", "iFen", "oFen")) # Clean up
 
-## Flag potential outliers in all numeric columns:
+# Flag potential outliers in all numeric columns:
 lapply(tests[ , -c(1, 2)], FUN = bpOutliers)
 
-###--------------------------------------------------------------------------###
+# > 2.2 Median Absolute Deviation method ---------------------------------------
 
-## Assign function arguments to demonstrate its internals:
+# Assign function arguments to demonstrate its internals:
 x     <- tests$SATV
 cut   <- 2.5
 na.rm <- TRUE
 
-## Define a function to implement the MAD method:
+# Define a function to implement the MAD method:
 madOutliers <- function(x, cut = 2.5, na.rm = TRUE) {
-    ## Compute the median and MAD of x:
+    # Compute the median and MAD of x:
     mX   <- median(x, na.rm = na.rm)
     madX <- mad(x, na.rm = na.rm)
     
-    ## Return row indices of observations for which |T_MAD| > cut:
+    # Return row indices of observations for which |T_MAD| > cut:
     which(abs(x - mX) / madX > cut)
 } 
 
+# Remove objects used to demonstrate what happens inside the function
 rm(list = c("x", "cut", "na.rm", "mX", "madX")) # Clean up
 
-## Find potential outliers in all numeric columns:
+# Find potential outliers in all numeric columns:
 lapply(tests[ , -c(1, 2)], FUN = madOutliers)
 
-###--------------------------------------------------------------------------###
+# 3. Outlier analysis (multivariate) -------------------------------------------
+#    Using the Robust Mahalanobis distance method
 
-## Assign function arguments to demonstrate its internals:
+# Assign function arguments to demonstrate its internals:
 data  <- tests[ , -c(1, 2)]
 prob  <- 0.99
 ratio <- 0.75
 seed  <- 235711
 
-## Define a function to implement a robust version of Mahalanobis distance using
-## MCD estimation:
+# Define a function to implement a robust version of Mahalanobis distance using
+# MCD estimation:
 mcdMahalanobis <- function(data, prob, ratio = 0.75, seed = NULL) {
-    ## Set a seed, if one is provided:
+    # Set a seed, if one is provided:
     if(!is.null(seed)) set.seed(seed)
     
-    ## Compute the MCD estimates of the mean and covariance matrix:
+    # Compute the MCD estimates of the mean and covariance matrix:
     stats <- cov.mcd(data, quantile.used = floor(ratio * nrow(data)))
     
-    ## Compute robust squared Mahalanobis distances
+    # Compute robust squared Mahalanobis distances
     md <- mahalanobis(x = data, center = stats$center, cov = stats$cov)
     
-    ## Find the cutoff value:
+    # Find the cutoff value:
     crit <- qchisq(prob, df = ncol(data))
     
-    ## Return row indices of flagged observations:
+    # Return row indices of flagged observations:
     which(md > crit)
 }
 
+# Remove objects used to demonstrate what happens inside the function
 rm(list = c("data", "prob", "ratio", "stats", "md", "crit")) # Clean up
 
-## Flag potential multivariate outliers:
+# Flag potential multivariate outliers:
 mcdMahalanobis(data = tests[ , -c(1, 2)], prob = 0.99, seed = 235711)
 
--- week-2-lab-1/code/lab1_practice.R
+++ week-2-lab-1/code/lab1_practice.R
@@ -1,156 +1,175 @@
-### Title:    Stats & Methods Lab 1 Practice Script
-### Authors:  Kyle M. Lang, L.V.D.E. Vogelsmeier, Edo
-### Created:  2018-04-10
-### Modified: 2022-02-03
+# Title:    Stats & Methods Lab 1 Practice Script
+# Authors:  Kyle M. Lang, L.V.D.E. Vogelsmeier, Edo
+# Created:  2018-04-10
+# Modified: 2023-01-10
 
+# General instructions ---------------------------------------------------------
 
-###          ###
-### Overview ###
-###          ###
+# - By completing the tasks in this script, you will practice:
+#   a. inferential testing,
+#   b. basic EDA techniques,
+#   c. missing data descriptives,
+#   d. univariate outlier analysis
+#   e. multivariate outlier analysis
+# - You will need four datasets:
+#   1. "bfi_clean.rds",
+#   2. "tests.rds",
+#   3. "bfiOE.rds",
+#   4. "airQual.rds".
+#   These datasets are saved in the "data" directory for this set of lab
+#   materials.
 
-## You will practice inferential testing, some basic EDA techniques, missing
-## data descriptives, and outlier analysis.
+# Preliminaries ----------------------------------------------------------------
 
-## You will need four datasets to complete the following tasks: "bfi_clean.rds",
-## "tests.rds", "bfiOE.rds", and "airQual.rds". These datasets are saved in the
-## "data" directory for this set of lab materials.
+# > Tasks and questions --------------------------------------------------------
 
+# 1) If you have not already done so, use the "install.packages" function to
+#    install the "mice" package.
 
-###                   ###
-### Tasks / Questions ###
-###                   ###
+# 2) Use the "library" function to load the "mice" and "MASS" packages.
 
+# 3) Use the "paste0" function and the "readRDS" function to load the four
+#    datasets into memory.
 
-##--Preliminaries-------------------------------------------------------------##
+# Testing ----------------------------------------------------------------------
 
-## 1) If you have not already done so, use the "install.packages" function to
-##    install the "mice" package.
+# > Instructions ---------------------------------------------------------------
 
-## 2) Use the "library" function to load the "mice" and "MASS" packages.
+# - Use the "bfi_clean" data to answer the questions in this section.
 
-## 3) Use the "paste0" function and the "readRDS" function to load the four
-##    datasets into memory.
+# > Tasks and questions --------------------------------------------------------
 
+# 4a) Conduct a t-test to check for gender differences in mean levels of
+#     "agree" (i.e., agreeableness). Do not assume equal variances.
+# 4b) What is the value of the estimated mean difference in "agree"?
+# 4c) What is the value of the estimated t-statistic?
+# 4d) Is the estimated mean difference significant at the alpha = 0.05 level?
 
-##--Testing-------------------------------------------------------------------##
+# 5a) Conduct a t-test to check for gender differences in mean levels of
+#     "agree" (i.e., agreeableness). Assume equal variances.
+# 5b) What is the value of the estimated mean "agree" for males?
+# 5c) What is the value of the estimated t-statistic?
+# 5d) What is the 95% CI for the estimated mean difference?
+# 5e) Is the t-statistic you found here different from the one you computed in
+#     Q1? If so, why?
 
-### Use the "bfi_clean" data to complete the following analyses/tasks:
+# 6a) Test for a significant Pearson correlation between "agree" and "neuro"
+#    (i.e., neuroticism).
+# 6b) What is the value of the correlation coefficient?
+# 6c) Is this correlation significantly different from zero at the alpha = 0.05
+#     level?
 
-## 4a) Conduct a t-test to check for gender differences in mean levels of
-##     "agree" (i.e., agreeableness). Do not assume equal variances.
-## 4b) What is the value of the estimated mean difference in "agree"?
-## 4c) What is the value of the estimated t-statistic?
-## 4d) Is the estimated mean difference significant at the alpha = 0.05 level?
+# 7a) Test the hypothesis that the correlation between "consc"
+#     (i.e., conscientiousness) and "neuro" is less than zero.
+# 7b) What is the value of the estimated correlation coefficient?
+# 7c) What is the 95% CI for the estimated correlation coefficient?
+# 7d) Is this correlation significantly less than zero at the alpha = 0.05
+#     level?
 
-## 5a) Conduct a t-test to check for gender differences in mean levels of
-##     "agree" (i.e., agreeableness). Assume equal variances.
-## 5b) What is the value of the estimated mean "agree" for males?
-## 5c) What is the value of the estimated t-statistic?
-## 5d) What is the 95% CI for the estimated mean difference?
-## 5e) Is the t-statistic you found here different from the one you computed in
-##     Q1? If so, why?
+# Exploratory data analysis (EDA) ----------------------------------------------
 
-## 6a) Test for a significant Pearson correlation between "agree" and "neuro"
-##    (i.e., neuroticism).
-## 6b) What is the value of the correlation coefficient?
-## 6c) Is this correlation significantly different from zero at the alpha = 0.05
-##     level?
+# > Instructions ---------------------------------------------------------------
 
-## 7a) Test the hypothesis that the correlation between "consc"
-##     (i.e., conscientiousness) and "neuro" is less than zero.
-## 7b) What is the value of the estimated correlation coefficient?
-## 7c) What is the 95% CI for the estimated correlation coefficient?
-## 7d) Is this correlation significantly less than zero at the alpha = 0.05
-##     level?
+# - Use the "tests" data to answer the questions in this section.
 
-##--EDA-----------------------------------------------------------------------##
+# > Tasks and questions --------------------------------------------------------
 
-### Use the "tests" data to answer the following questions:
+# 8) What are the dimensions of these data?
 
-## 8) What are the dimensions of these data?
+# 9) What is the mean "SATQ" score?
 
-## 9) What is the mean "SATQ" score?
+# 10) What is the variance of the "SATQ" scores?
 
-## 10) What is the variance of the "SATQ" scores?
+# 11) What is the median "SATV" score?
 
-## 11) What is the median "SATV" score?
+# 12) Create a histogram of the "ACT" variable.
 
-## 12) Create a histogram of the "ACT" variable.
+# 13) Create a kernel density plot of the "ACT" variable.
 
-## 13) Create a kernel density plot of the "ACT" variable.
+# 14) Overlay a normal density on top of the "ACT" histogram.
 
-## 14) Overlay a normal density on top of the "ACT" histogram.
+# 15) Create a grouped boxplot that plots "ACT" by "education".
 
-## 15) Create a grouped boxplot that plots "ACT" by "education".
+# 16) Create a frequency table of "education".
 
-## 16) Create a frequency table of "education".
+# 17) Create a contingency table that cross-classifies "gender" and
+#     "education".
 
-## 17) Create a contingency table that cross-classifies "gender" and
-##     "education".
+# 18) Suppose a certain university admits any student with an ACT score of, at
+#     least, 25. How many of the women in the "tests" data would be admitted?
 
-## 18) Suppose a certain university admits any student with an ACT score of, at
-##     least, 25. How many of the women in the "tests" data would be admitted?
+# Missing Data Descriptives ----------------------------------------------------
 
+# > Instructions ---------------------------------------------------------------
 
-##--Missing Data Descriptives-------------------------------------------------##
+# - Use the "bfiOE" data to answer the questions in this section.
 
-### Use the "bfiOE" data to answer the following questions:
+# > Tasks and questions --------------------------------------------------------
 
-## 19a) Compute the proportion of missing values for each variable.
-## 19b) What is the percentage of missing data for "O1"?
+# 19a) Compute the proportion of missing values for each variable.
+# 19b) What is the percentage of missing data for "O1"?
 
-## 20a) Compute the number of observed values for each variable:
-## 20b) What is the number of observed values for "E1"?
+# 20a) Compute the number of observed values for each variable:
+# 20b) What is the number of observed values for "E1"?
 
-## 21a) Compute the covariance coverage matrix.
-## 21b) What is the range of covariance coverage values?
-## 21c) What is the covariance coverage between "E2" and "O4"?
-## 21d) How many unique covariance coverages are less than 0.75?
+# 21a) Compute the covariance coverage matrix.
+# 21b) What is the range of covariance coverage values?
+# 21c) What is the covariance coverage between "E2" and "O4"?
+# 21d) How many unique covariance coverages are less than 0.75?
 
-## 22a) Compute the missing data patterns for these data.
-## 22b) How many distinct missing data patterns exist in these data?
-## 22c) How many missing data patterns have only one missing value?
-## 22d) How many observations are affected by patterns that involve only one
-##      missing value?
+# 22a) Compute the missing data patterns for these data.
+# 22b) How many distinct missing data patterns exist in these data?
+# 22c) How many missing data patterns have only one missing value?
+# 22d) How many observations are affected by patterns that involve only one
+#      missing value?
 
+# Univariate outliers ----------------------------------------------------------
 
-##--Outliers------------------------------------------------------------------##
+# > Instructions ---------------------------------------------------------------
 
-### NOTE: You can use the functions provided in the demonstrations script to
-###       complete the following tasks.
+# - Use the "airQual" data to answer the questions in this section.
+# - You can use the functions provided in the demonstrations script to
+#   complete the following tasks.
 
-### Use the "airQual" data to complete the following:
+# > Tasks and questions --------------------------------------------------------
 
-## 23a) Use Tukey's boxplot method to find possible and probable outliers in the
-##     "Ozone", "Solar.R", "Wind", and "Temp" variables.
-## 23b) Did you find any possible outliers?
-## 23c) Did you find any probable outliers?
-## 23d) Which, if any, observations were possible outliers on "Ozone"?
-## 23e) Which, if any, observations were probable outliers on "Wind"?
+# 23a) Use Tukey's boxplot method to find possible and probable outliers in the
+#      "Ozone", "Solar.R", "Wind", and "Temp" variables.
+# 23b) Did you find any possible outliers?
+# 23c) Did you find any probable outliers?
+# 23d) Which, if any, observations were possible outliers on "Ozone"?
+# 23e) Which, if any, observations were probable outliers on "Wind"?
 
-## 24a) Use the MAD method (with a cutoff of 3) to find potential outliers in
-##     the "Ozone", "Solar.R", "Wind", and "Temp" variables.
-## 24b) Did you find any potential outliers?
-## 24c) Which, if any, observations are potential outliers on "Wind"?
+# 24a) Use the MAD method (with a cutoff of 3) to find potential outliers in
+#      the "Ozone", "Solar.R", "Wind", and "Temp" variables.
+# 24b) Did you find any potential outliers?
+# 24c) Which, if any, observations are potential outliers on "Wind"?
 
-### For Question 3, you will use different parameterizations of robust
-### Mahalanobis distance (with MCD estimation) to check for multivariate
-### outliers on the "Ozone", "Solar.R", "Wind", and "Temp" variables.
+# Multivariate outliers --------------------------------------------------------
 
-### NOTE: When running the mcdMahaonobis() function, set the seed to "235711".
+# > Instructions ---------------------------------------------------------------
 
-## 25a) Which, if any, observations are flagged as multivariate outliers when
-##     using 75% of the sample for the MCD estimation and using a probability of
-##     0.99 for the cutoff value?
-## 25b) Which, if any, observations are flagged as multivariate outliers when
-##     using 75% of the sample for the MCD estimation and using a probability of
-##     0.999 for the cutoff value?
-## 25c) Which, if any, observations are flagged as multivariate outliers when
-##     using 50% of the sample for the MCD estimation and using a probability of
-##     0.99 for the cutoff value
-## 25d) Which, if any, observations are flagged as multivariate outliers when
-##     using 50% of the sample for the MCD estimation and using a probability of
-##     0.999 for the cutoff value?
-## 25e) Based on the above, what consequences do you observe when changing the
-##     fraction of the sample used for MCD estimation and when changing the
-##     cutoff probability?
+# - Use the "airQual" data to answer the questions in this section.
+# - Use the robust MCD Mahalanobis distance to check for multivariate
+#   outliers on the "Ozone", "Solar.R", "Wind", and "Temp" variables.
+# - You can use the functions provided in the demonstrations script to
+#   complete the following tasks.
+# - When running the mcdMahaonobis() function, set the seed to "235711".
+
+# > Tasks and questions --------------------------------------------------------
+
+# 25a) Which, if any, observations are flagged as multivariate outliers when
+#      using 75% of the sample for the MCD estimation and using a probability of
+#      0.99 for the cutoff value?
+# 25b) Which, if any, observations are flagged as multivariate outliers when
+#      using 75% of the sample for the MCD estimation and using a probability of
+#      0.999 for the cutoff value?
+# 25c) Which, if any, observations are flagged as multivariate outliers when
+#      using 50% of the sample for the MCD estimation and using a probability of
+#      0.99 for the cutoff value
+# 25d) Which, if any, observations are flagged as multivariate outliers when
+#      using 50% of the sample for the MCD estimation and using a probability of
+#      0.999 for the cutoff value?
+# 25e) Based on the above, what consequences do you observe when changing the
+#      fraction of the sample used for MCD estimation and when changing the
+#      cutoff probability?

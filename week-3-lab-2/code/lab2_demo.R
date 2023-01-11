714cb8f8349cd6dd9d9b65213f516cfc1485b1ed
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-01-11 18:04:45 +0100

    hk: improve formatting week 3 lab 3

--- week-3-lab-2/code/lab2_demo.R
+++ week-3-lab-2/code/lab2_demo.R
@@ -1,109 +1,105 @@
-### Title:    Stats & Methods Lab 2 Demonstration Script
-### Author:   Kyle M. Lang
-### Created:  2017-09-08
-### Modified: 2020-09-09
+# Title:    Stats & Methods Lab 2 Demonstration Script
+# Author:   Kyle M. Lang
+# Created:  2017-09-08
+# Modified: 2023-01-11
 
-###--------------------------------------------------------------------------###
+# Set up -----------------------------------------------------------------------
 
-### Preliminaries ###
+# Make sure workspace is clean
+rm(list = ls(all = TRUE))
 
+# Install required packages
 install.packages("MLmetrics", repos = "http://cloud.r-project.org")
 
-rm(list = ls(all = TRUE)) # Clear workspace
-
+# Load required packages
 library(MLmetrics) # We'll need this for MSEs
 
-setwd("") # Let's all set our working directory to the correct place
+# Make sure the working directory is set to the location of this file
+setwd("")
 
-###--------------------------------------------------------------------------###
-
-### Data I/O ###
-
+# Define data location
 dataDir <- "../data/"
 
+# Load the mtcars dataset from the lab folder
 cars <- readRDS(paste0(dataDir, "mtcars.rds"))
 
-###--------------------------------------------------------------------------###
+# 1. Simple Linear Regression --------------------------------------------------
 
-### Simple Linear Regression ###
-
-## Fit a simple linear regression model:
+# Fit a simple linear regression model:
 out1 <- lm(qsec ~ hp, data = cars)
 
-## See what's inside the fitted lm object:
+# See what's inside the fitted lm object:
 ls(out1)
 
-## Summarize the results:
+# Summarize the results:
 summary(out1)
 
-## The 'summary' output is an object that we can save to a variable:
+# The 'summary' output is an object that we can save to a variable:
 s1 <- summary(out1)
 ls(s1)
 s1
 
-## Access the R^2 slot in the summary object:
+# Access the R^2 slot in the summary object:
 s1$r.squared
 
-## Extract coefficients:
+# Extract coefficients:
 coef(out1)
 
-## Extract residuals:
+# Extract residuals:
 res3 <- resid(out1)
 res3
 
-## Extract fitted values (two ways):
+# Extract fitted values (two ways):
 yHat1.1 <- fitted(out1)
 yHat1.2 <- predict(out1)
 
 yHat1.1
 yHat1.2
 
-## Compare:
+# Compare:
 yHat1.1 - yHat1.2
 
-###--------------------------------------------------------------------------###
+# > 1.1 Reproduce the inferential tests from Lab 1 -----------------------------
 
-## Reproduce the Tests from Lab 1 ###
-
-## t-test:
+# t-test
 t.test(mpg ~ am, data = cars, var.equal = TRUE)
+
+# t-test with linear regression
 summary(lm(mpg ~ am, data = cars))
 
-## Correlation:
+# correlation test:
 with(cars, cor.test(mpg, wt))
 
-## A naive approach:
+# correlation test w/ linear regression (naive)
 summary(lm(mpg ~ wt, data = cars))
 
-## We need to standardize the regression coefficients:
+# correlation test w/ linear regression (standardized coefs)
 summary(lm(scale(mpg) ~ scale(wt), data = cars))
 
-###--------------------------------------------------------------------------###
+# 2. Multiple Linear Regression ------------------------------------------------
 
-### Multiple Linear Regression ###
-
-## Fit a multiple linear regression model:
+# Fit a multiple linear regression model:
 out4 <- lm(qsec ~ hp + wt + carb, data = cars)
 
-## Summarize the model:
+# Summarize the model:
 s4 <- summary(out4)
 s4
 
-## Extract R^2:
+# Extract R^2:
 s4$r.squared
 
-## Extract F-stat:
+# Extract F-stat:
 s4$fstatistic
 
-## Extract coefficients:
+# Extract coefficients:
 coef(out4)
 
-## Compute confidence intervals for coefficients:
+# Compute confidence intervals for coefficients:
 confint(out4)
 confint(out4, parm = "hp")
 confint(out4, parm = c("(Intercept)", "wt"), level = 0.99)
 
-## Manually compute t-statistics:
+# Manually compute t-statistics:
 cf4 <- coef(out4)
 se4 <- sqrt(diag(vcov(out4)))
 t4  <- cf4 / se4
@@ -111,49 +107,48 @@ t4  <- cf4 / se4
 s4
 t4
 
-###--------------------------------------------------------------------------###
+# > 2.1 Model comparison -------------------------------------------------------
 
-### Model Comparison ###
-
-## Change in R^2:
+# Change in R^2:
 s4$r.squared - s1$r.squared
 
-## Significant increase in R^2?
+# Significant increase in R^2?
 anova(out1, out4)
 
-## Compare MSE values:
+# Compare MSE values:
 mse1 <- MSE(y_pred = predict(out1), y_true = cars$qsec)
 mse4 <- MSE(y_pred = predict(out4), y_true = cars$qsec)
 
 mse1
 mse4
 
-###--------------------------------------------------------------------------###
+# > 2.2 Model building in R ----------------------------------------------------
 
-### A More Elegant Way to Build Models ###
-
-## The long way:
+# Define a starting model
 out1 <- lm(qsec ~ hp, data = mtcars)
+
+# Update the model, the long way:
 out2 <- lm(qsec ~ hp + wt, data = mtcars)
 out3 <- lm(qsec ~ hp + wt + gear + carb, data = mtcars)
 
-## The short way:
+# Update the model, the short way:
 out2.1 <- update(out1, ". ~ . + wt")
 out3.1 <- update(out2.1, ". ~ . + gear + carb")
 out3.2 <- update(out1, ". ~ . + wt + gear + carb")
 
-## We can also remove variables:
+# We can also remove variables:
 out1.1 <- update(out2, ". ~ . - wt")
 
-summary(out2)
-summary(out2.1)
+# Compare models
+out2
+out2.1
 
 all.equal(out1, out1.1)
 all.equal(out2, out2.1)
 all.equal(out3, out3.1)
 all.equal(out3, out3.2)
 
-## We can rerun the same model on new data:
+# We can rerun the same model on new data:
 mtcars2 <- mtcars[1 : 15, ]
 
 out4 <- update(out3, data = mtcars2)
--- week-3-lab-2/code/lab2_practice.R
+++ week-3-lab-2/code/lab2_practice.R
@@ -1,93 +1,91 @@
-### Title:    Stats & Methods Lab 2 Practice Script
-### Author:   Kyle M. Lang, L.V.D.E.Vogelsmeier, Edo
-### Created:  2018-04-10
-### Modified: 2022-02-15
+# Title:    Stats & Methods Lab 2 Practice Script
+# Author:   Kyle M. Lang, L.V.D.E.Vogelsmeier, Edo
+# Created:  2018-04-10
+# Modified: 2023-01-11
 
+# General instructions ---------------------------------------------------------
 
-###          ###
-### Overview ###
-###          ###
+# - By completing the tasks in this script, you will practice:
+#   a. simple and multiple linear regression,
+#   b. model comparison,
+# - You will need a single data set: "longley.rds"
+#   IMPORTANT NOTE:
+#   You have to load the data from the folder as indicated here. R has a "longley"
+#   dataset as well but, if you use this, it will lead to different results because
+#   it differs a bit from the one in the folder. As it is a build-in dataset,
+#   the function lm(GNP ~ Year, data = longley) would even work without loading the
+#   build-in data. If you use the wrong dataset and obtain wrong results, you will
+#   not get back lost points for corresponding questions in Quiz 3. Thus, please
+#   make sure to load the data from the folder and use it for all analyses.
 
-## You will practice basic regression modeling and model comparison.
+# Preliminaries ----------------------------------------------------------------
 
-## You will need the "longley.rds" dataset. This dataset is available in the
-## "data" directory for this lab.
+# > Tasks and questions --------------------------------------------------------
 
-## IMPORTANT NOTE:
-## You have to load the data from the folder as indicated here. R has a "longley" 
-## dataset as well but, if you use this, it will lead to different results because 
-## it differs a bit from the one in the folder. As it is a build-in dataset, 
-## the function lm(GNP ~ Year, data = longley) would even work without loading the 
-## build-in data. If you use the wrong dataset and obtain wrong results, you will 
-## not get back lost points for corresponding questions in Quiz 3. Thus, please 
-## make sure to load the data from the folder and use it for all analyses.
+# 1) Use the "install.packages" function to install the "MLmetrics" package.
 
+# 2) Use the "library" function to load the "MLmetrics" packages.
 
-###                   ###
-### Tasks / Questions ###
-###                   ###
+# 3) Use the "paste0" function and the "readRDS" function to load the
+#    "longley.rds" data into your workspace.
 
+# Linear regression ------------------------------------------------------------
 
-##--Preliminaries-------------------------------------------------------------##
+# > Instructions ---------------------------------------------------------------
 
-## 1) Use the "install.packages" function to install the "MLmetrics" package.
+# - Use the "longley" data to answer the questions in this section.
 
-## 2) Use the "library" function to load the "MLmetrics" packages.
+# > Tasks and questions --------------------------------------------------------
 
-## 3) Use the "paste0" function and the "readRDS" function to load the
-##    "longley.rds" data into your workspace.
+# 4a) Regress "GNP" onto "Year".
+# 4b) What is the effect of "Year" on "GNP"?
+# 4c) Is the effect of "Year" on "GNP" statistically significant at the
+#     alpha = 0.05 level?
+# 4d) Is the effect of "Year" on "GNP" statistically significant at the
+#     alpha = 0.01 level?
 
+# 5a) Regress "GNP" onto "Year" and "Population".
+# 5b) Is there a significant effect (at the alpha = 0.05 level) of "Year" on
+#     "GNP", after controlling for "Population"? 
+# 5c) Is there a significant effect (at the alpha = 0.01 level) of "Year" on
+#     "GNP", after controlling for "Population"? 
+# 5d) What is the 95% confidence interval (CI) for the partial effect of
+#     "Population" on "GNP"?
 
-##--Linear Regression---------------------------------------------------------##
+# 6a) Regress "GNP" onto "Year" and "Employed".
+# 6b) What is the partial effect of "Employed" on "GNP", after controlling for
+#     "Year"? 
+# 6c) Is the partial effect of "Employed" on "GNP" statistically significant at
+#     the alpha = 0.05 level?
+# 6d) What is the 99% confidence interval (CI) for the partial effect of
+#     "Employed" on "GNP"?
 
-### Use the "longley" data for the following:
+# 7a) Regress "GNP" onto "Year" and "Unemployed".
+# 7b) What is the partial effect of "Unemployed" on "GNP", after controlling
+#     for "Year"? 
+# 7c) Is the partial effect of "Unemployed" on "GNP" statistically significant
+#     at the alpha = 0.05 level?
 
-## 4a) Regress "GNP" onto "Year".
-## 4b) What is the effect of "Year" on "GNP"?
-## 4c) Is the effect of "Year" on "GNP" statistically significant at the
-##     alpha = 0.05 level?
-## 4d) Is the effect of "Year" on "GNP" statistically significant at the
-##     alpha = 0.01 level?
+# Model Comparison ------------------------------------------------------------
 
-## 5a) Regress "GNP" onto "Year" and "Population".
-## 5b) Is there a significant effect (at the alpha = 0.05 level) of "Year" on
-##     "GNP", after controlling for "Population"? 
-## 5c) Is there a significant effect (at the alpha = 0.01 level) of "Year" on
-##     "GNP", after controlling for "Population"? 
-## 5d) What is the 95% confidence interval (CI) for the partial effect of
-##     "Population" on "GNP"?
+# > Instructions ---------------------------------------------------------------
 
-## 6a) Regress "GNP" onto "Year" and "Employed".
-## 6b) What is the partial effect of "Employed" on "GNP", after controlling for
-##     "Year"? 
-## 6c) Is the partial effect of "Employed" on "GNP" statistically significant at
-##     the alpha = 0.05 level?
-## 6d) What is the 99% confidence interval (CI) for the partial effect of
-##     "Employed" on "GNP"?
+# - Use the "longley" data to answer the questions in this section.
 
-## 7a) Regress "GNP" onto "Year" and "Unemployed".
-## 7b) What is the partial effect of "Unemployed" on "GNP", after controlling
-##     for "Year"? 
-## 7c) Is the partial effect of "Unemployed" on "GNP" statistically significant
-##     at the alpha = 0.05 level?
+# > Tasks and questions --------------------------------------------------------
 
+# 8a) What is the difference in R-squared between the simple linear regression
+#     of "GNP" onto "Year" and the multiple linear regression of "GNP" onto
+#     "Year" and "Population"?
+# 8b) Is this increase in R-squared significantly different from zero at the
+#     alpha = 0.05 level? 
+# 8c) What is the value of the test statistic that you used to answer (8b)?
 
-##--Model Comparison----------------------------------------------------------##
-
-### Use the "longley" data for the following:
-
-## 8a) What is the difference in R-squared between the simple linear regression
-##     of "GNP" onto "Year" and the multiple linear regression of "GNP" onto
-##     "Year" and "Population"?
-## 8b) Is this increase in R-squared significantly different from zero at the
-##     alpha = 0.05 level? 
-## 8c) What is the value of the test statistic that you used to answer (8b)?
-
-## 9a) What is the MSE for the model that regresses "GNP" onto "Year" and
-##     "Employed"?
-## 9b) What is the MSE for the model that regresses "GNP" onto "Year" and
-##     "Unemployed"?
-## 9c) According to the MSE values calculated above, is "Employed" or
-##     "Unemployed" the better predictor of "GNP"?
-## 9d) Could we do the comparison in (9c) using an F-test for the difference in
-##     R-squared values? Why or why not?
+# 9a) What is the MSE for the model that regresses "GNP" onto "Year" and
+#     "Employed"?
+# 9b) What is the MSE for the model that regresses "GNP" onto "Year" and
+#     "Unemployed"?
+# 9c) According to the MSE values calculated above, is "Employed" or
+#     "Unemployed" the better predictor of "GNP"?
+# 9d) Could we do the comparison in (9c) using an F-test for the difference in
+#     R-squared values? Why or why not?
